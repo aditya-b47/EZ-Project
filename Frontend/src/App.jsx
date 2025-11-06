14d4b2283b767343888fcdcca78c975ea49b0c76
eddy699 <barryallenakaflash995@gmail.com>
2025-11-07 03:47:42 +0530

    msg

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 
 import './App.css'
+import ContactForm from './components/ContactForm'
+import Hero from './components/hero'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     {/* <ContactForm/> */}
+     <Hero/>
     </>
   )
 }
